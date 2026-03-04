61aee8f6b1772bd9c4df42a639d974cfb9f4d36f
Olegg000 <dfsagbnkjfnplsoi@gmail.com>
2026-03-04 13:06:00 +0400

    changes21

--- shared/src/iosMain/kotlin/com/example/pgk_food/shared/data/local/AppDatabaseConstructor.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pgk_food.shared.data.local
-
-import androidx.room.RoomDatabaseConstructor
-
-actual object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
-    actual override fun initialize(): AppDatabase {
-        return createAppDatabaseBuilder().build()
-    }
-}
